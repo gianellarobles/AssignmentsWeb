2392fbd62a846cf8a9efa62e3856d7558d6ccff4
gianellarobles <gianellarobles@gmail.com>
2023-11-30 14:32:16 -0500

    model folder updates

--- client/src/model/session.ts
+++ client/src/model/session.ts
@@ -2,33 +2,47 @@
 */
 import { reactive } from "vue";
 import { useRouter } from "vue-router"
+import { useToast } from "vue-toastification";
+import * as myFetch from "./myFetch";
 import { type User, getUserByEmail } from "./users";
+
+const toast = useToast();
 
 const session = reactive({
   user: null as User | null,
   redirectUrl: null as string | null,
+  messages: [] as {
+    type: string,
+    text: string
+  }[],
+  loading: 0
 })
 
-
+export function api(action: string, body?: unknown, method?: string){
+  session.loading++;
+  return myFetch.api(`${action}`, body, method)
+    .catch(err=> showError(err))
+    .finally(()=> session.loading--);
+}
 
 export function getSession(){
   return session;
+}
+
+export function showError(err: any){
+  console.error(err);
+  session.messages.push({ type: "error", text: err.message ?? err});
+  toast.error( err.message ?? err);
 }
 
 export function useLogin(){
   const router = useRouter();
 
   return {
-    login(email: string, password: string): User | null {
-      const user = getUserByEmail(email);
-      if(user && user.password === password){
-        session.user = user;
-
-        router.push(session.redirectUrl || "/");
-
-        return user;
-      }
-      return null;
+    async login(email: string, password: string): Promise< User | null> {
+      session.user = await api("users/login", { email, password });
+      router.push(session.redirectUrl || "/");
+      return session.user;
     },
     logout(){
       session.user = null;
--- client/src/model/shoppingCart.ts
+++ client/src/model/shoppingCart.ts
@@ -1,44 +1,50 @@
+
 /* B"H
 */
+import { computed, reactive } from "vue";
 import { type Product } from "@/model/products";
-import { reactive } from "vue";
 
 export type ShoppingCartItem = {
-    product: Product;
-    quantity: number;
+  product: Product;
+  quantity: number;
+};
 
-}
-
-export count cart = reactive([] as ShoppingCartItem[]);
+const cart = reactive([] as ShoppingCartItem[]);
 
 export const getCart = () => cart;
 
 export const addToCart = (product: Product) => {
-    count item = cart.find((item) => item.product.id === product.id);
-
-    if (item) {
-        item.quantity++;
-    } else {
-        cart.push({ product, quantity: 1 });
-    }
+  const item = cart.find(item => item.product.id === product.id);
+  if (item) {
+    item.quantity++;
+  } else {
+    cart.push({ product, quantity: 1 });
+  }
 };
 
 export const removeFromCart = (product: Product, quantity?: number) => {
-    const item = cart.find(item => item.product.id === product.id);
-    if (item) {
-        quantity = quantity ?? item.quantity;
-        item.quantity--;
-    }
+  const item = cart.find(item => item.product.id === product.id);
+  if (item) {
+    quantity = quantity ?? item.quantity;
+    item.quantity -= quantity;
     if (item.quantity <= 0) {
-        const index = cart.indexOf(item);
-        cart.splice(index, 1);
-    };
-}
+      const index = cart.indexOf(item);
+      cart.splice(index, 1);
+    }
+  }
+};
 
-export count clearCart = () => {
-    cart.splice(0, cart.length);
-}
+export const clearCart = () => {
+  cart.splice(0, cart.length);
+};
 
-export count total = () => cart.reduce((sum, item) => sum + cart.product.price * item.quantity, 0); 
+export const total = computed( () => cart.reduce((sum, item) => sum + item.product.price * item.quantity, 0) );
+export const count = computed( () => cart.reduce((sum, item) => sum + item.quantity, 0) );
 
 
+/* Main functional functions
+  filter
+  find
+  map
+  reduce
+*/
